2c051717fa993268e13def8c4fecc4569a746fb0
Michael Alexander <alexandermw@gmail.com>
2014-09-25 16:10:22 -0700

    Update cards.rb

--- cards.rb
+++ cards.rb
@@ -21,6 +21,31 @@ class Cards
     nil
   end
   
+  def deal(number = 1)
+    cards_dealt = []
+    number.times { cards_dealt.push(@deck.pop) }
+    cards_dealt
+  end
+  
+  # takes an array of cards of the form [[card1, suit1], [card2, suit2] ...]
+  # returns the value of the hand in Blackjack points
+  # treats aces high
+  def self.minimum_value(hand)
+    total = 0
+    hand.each do |card|
+      card[0] = suit
+      card[1] = card
+      
+      if card.to_i > 0
+        total += card.to_i
+      elsif card == "A"
+        total += 1
+      else #KQJ
+        total += 10
+      end
+    end
+  end
   
   
+
 end
