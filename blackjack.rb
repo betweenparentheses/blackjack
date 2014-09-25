200fd3d9c700dfb3ebfde2e2f6c1c9eb3967f768
Michael Alexander <alexandermw@gmail.com>
2014-09-25 16:41:59 -0700

    Update blackjack.rb

--- blackjack.rb
+++ blackjack.rb
@@ -2,3 +2,43 @@ require 'player'
 require 'cards'
 require 'dealer'
 
+class Blackjack
+  
+  def initialize
+    @cards = Cards.new
+    @player1 = Player.new(@cards)
+    @dealer = Dealer.new(@cards)
+  end
+  
+  def play_hand
+    player = @player1
+    
+    until both_stay? || blackjack? do
+      
+      render_cards
+      player.take_turn
+      break if bust?
+      dealer.take_turn
+      break if bust?
+    end
+    
+    render_cards
+    find_winners
+    #determine winnings
+  end
+  
+  
+  def find_winners
+  end
+  
+  def both_stay?
+  end
+  
+  def blackjack?
+
+  end
+  
+  def bust?
+  end
+  
+end
